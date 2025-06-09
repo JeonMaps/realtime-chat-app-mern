0884d7e316f2c14cfba5c0a9a94cd9e33f2d9e38
Jeon Maps <jeonmapatac@gmail.com>
2025-06-09 22:26:24 +0800

    Sign up functionality
    -signup funciton in useAuthStore()
    -installed lucide-react for icons
    -installed cors for to specify frontend origin url
    -Added guard for each route, navigate to home or login page depending on authentication status
    -Added validations and toaster for errors/success messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,30 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log('Server is running on port:' + PORT)
-    connectDB()
-})
+    console.log("Server is running on port:" + PORT);
+    connectDB();
+});
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,22 +2,35 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 
 export const useAuthStore = create((set) => ({
-    authUser: null,
-    isSigningUp: false,
-    isLoggingIn: false,
-    isUpdatingProfile: false,
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
 
-    isCheckingAuth: true,
+  isCheckingAuth: true,
 
-    checkAuth: async () => {
-        try {
-            const res = await axiosInstance.get("/auth/check");
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
 
-            set({ authUser: res.data });
-        } catch (error) {
-            set({ authUser: null });
-        } finally {
-            set({ isCheckingAuth: false });
-        }
-    },
+      set({ authUser: res.data });
+    } catch (error) {
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
 }));
